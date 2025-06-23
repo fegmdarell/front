53378e62e22808f709f793a8aa918ffd0771c390
fegmdarell <133925466+fegmdarell@users.noreply.github.com>
2025-06-23 18:46:45 -0500

    Corrijo import de logo en Landing.jsx y agrego imagen correcta

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.png'; // Asegúrate de que la ruta sea correcta
+import logo from '../assets/logo.jpg';
 
 export default function Landing() {
   const navigate = useNavigate();
